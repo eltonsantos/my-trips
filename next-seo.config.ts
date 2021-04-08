9091e443452f23b14dec759e76a11aca2e73027a
Elton Santos <eltonaxl@hotmail.com>
2021-04-08 04:14:18 -0300

    Change maps and seo

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -2,11 +2,11 @@ export default {
   openGraph: {
     type: 'website',
     locale: 'en_US',
-    url: 'https://my-trips.willianjusten.com.br',
+    url: 'https://my-trips-eltonsantos.vercel.com',
     site_name: 'My Trips'
   },
   twitter: {
-    handle: '@Willian_justen',
+    handle: '@eltin182',
     site: '@site',
     cardType: 'summary_large_image'
   }
