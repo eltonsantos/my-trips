aaeeee0f5dbaa7420cc53ae8493997a91904c04d
Elton Santos <eltonaxl@hotmail.com>
2021-04-10 20:28:33 -0300

    Add meta tag to google search console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="jlTIwHflkfMdbHpY5c_N4cP6u5GEAsqTcvXqkIliagk"
+        />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
